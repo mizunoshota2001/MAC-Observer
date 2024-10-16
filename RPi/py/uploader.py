8b30e8a78ed006f6b96ac8fda162e3a1e7a99b07
mizunoshota2001 <95399151+mizunoshota2001@users.noreply.github.com>
2024-10-17 00:41:47 +0900

    Refactor import statements in uploader.py

--- RPi/py/uploader.py
+++ RPi/py/uploader.py
@@ -1,7 +1,8 @@
 import time
 import json
-import requests
+import urllib.request
 from pathlib import Path
+
 HEADERS = {
     "Content-Type": "application/json",
 }
@@ -10,16 +11,21 @@ URL = "https://www.48v.me/~mizunoshota/cgi-bin/MAC-Observer/upload_devices.py/?%
 _5MINUTES = 5 * 60  # 5分を秒単位で定義
 
 __app_root = Path(__file__).parent.parent
-devices_json_path = __app_root/"data"/"devices.json"
+devices_json_path = __app_root / "data" / "devices.json"
 print(devices_json_path)
 
 
 def send():
-    if not devices_json_path.exists():
-        return
-    data = json.load(devices_json_path.open("r"))
-    res = requests.post(URL, headers=HEADERS, json=data)
-    print(res.text)
+    try:
+        if not devices_json_path.exists():
+            return
+        data = json.load(devices_json_path.open("r"))
+        data = json.dumps(data).encode('utf-8')
+        req = urllib.request.Request(URL, data=data, headers=HEADERS)
+        with urllib.request.urlopen(req) as res:
+            print(res.read().decode('utf-8'))
+    except Exception as e:
+        print(e)
 
 
 while True:
