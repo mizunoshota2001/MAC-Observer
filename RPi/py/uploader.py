f75312f55b74550cd3b497271e0adb05ac28d330
mizunoshota2001 <95399151+mizunoshota2001@users.noreply.github.com>
2024-10-17 10:21:45 +0900

    Refactor import statements in uploader.py and route.ts

--- RPi/py/uploader.py
+++ RPi/py/uploader.py
@@ -7,11 +7,13 @@ HEADERS = {
 PLACE = "1411"
 POST_TO = "https://www.48v.me/~mizunoshota/cgi-bin/MAC-Observer/upload_devices.py/?%s" % PLACE
 GET_FROM = "http://localhost:3000/api/getDevices"
+FLUSH = "http://localhost:3000/api/arpFlush"
 _5MINUTES = 5 * 60
 
 
 def send():
     try:
+        urllib.request.Request(FLUSH)
         req = urllib.request.Request(GET_FROM)
         body = urllib.request.urlopen(req)
         req = urllib.request.Request(
